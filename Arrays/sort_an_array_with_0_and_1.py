c2c41ff7a53332b8e2f5703b4249999176fd8581
Shrikar Reddy Kota <shrikar7766@gmail.com>
2021-09-04 09:57:08 +0530

    first problem

---
+++ Arrays/sort_an_array_with_0_and_1.py
@@ -0,0 +1,36 @@
+'''
+Problem statement:
+
+Sort an array of 0’s 1’s 2’s without using extra space or sorting algo 
+
+'''
+n = int(input())
+l = list(map(int, input().split()))
+
+#naive approach
+'''
+Count number of zeroes and ones. 
+'''
+zerocount = 0
+for i in range(n):
+    if l[i] == 0:
+        zerocount += 1
+for i in range(zerocount):
+    l[i] = 0
+for i in range(zerocount, n):
+    l[i] = 1
+
+print(l)
+
+#two pointer approach
+
+zeroptr = 0
+mover = 0
+
+for i in range(n):
+    if l[i] == 0:
+        l[zeroptr], l[mover] = l[mover], l[zeroptr]
+        zeroptr += 1
+    mover += 1
+
+print(l)
